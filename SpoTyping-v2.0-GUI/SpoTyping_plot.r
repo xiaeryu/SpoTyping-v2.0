b5349d89f9542708ff6f83447340dee9a7d018de
xiaeryu <xiaeryu@nus.edu.sg>
2015-11-25 16:37:19 +0800

    Update SpoTyping_plot.r

--- SpoTyping-v2.0-GUI/SpoTyping_plot.r
+++ SpoTyping-v2.0-GUI/SpoTyping_plot.r
@@ -1,15 +1,13 @@
 library(gdata)
 # pacakge gdata with function read.xls to parse xls files.
 
-args <- commandArgs(TRUE)
+inXLS <- ""
+outPDF <- ""
 
-## args[1]: the input excel file
-## args[2]: the output pdf file
-
-data <- read.xls(args[1])
+data <- read.xls(inXLS)
 data <- as.matrix(data)
 
-pdf(file=args[2])
+pdf(file=outPDF)
 for(i in 5:13){
     content <- data[(data[,i]!="" & !is.na(data[,i])),i]
     inpie <- table(content)
